b8efc13b7cfbc0d0504fff9e75f9d7f008ea7f00
moonwarmer <74154796+moonwarmer@users.noreply.github.com>
2020-12-11 16:58:05 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var deMenuButton = document.querySelector('header nav button');
+
+deMenuButton.addEventListener('click',openMenu);
+
+function openMenu() {
+  var hetMenu = document.querySelector('header nav ul');
+  hetMenu.classList.toggle('open');
+}
